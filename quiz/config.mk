a113eb2d9c794d90949c11f8e0c83cfa540fc1ff
aprotyas <aprotyas@u.rochester.edu>
2020-06-05 02:12:25 -0400

    Quiz

--- quiz/config.mk
+++ quiz/config.mk
@@ -1,7 +1,10 @@
 LANGUAGE=python
 COUNT_SRC=src/countwords.py
 COUNT_EXE=$(LANGUAGE) $(COUNT_SRC)
+PLOT_SRC=src/plotcounts.py
+PLOT_EXE=$(LANGUAGE) $(PLOT_SRC)
 ZIPF_SRC=src/testzipf.py
 ZIPF_EXE=$(LANGUAGE) $(ZIPF_SRC)
 TXT_FILES=$(wildcard books/*.txt)
 DAT_FILES=$(patsubst books/%.txt, %.dat, $(TXT_FILES))
+PNG_FILES=$(patsubst books/%.txt, %.png, $(TXT_FILES))
